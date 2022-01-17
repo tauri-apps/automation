82e8d45766a017a18503af724031da3df7aeb014
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-01-17 23:50:36 +0000

    Convert to javascript doc

---
+++ update-typescript-docs/index.js
@@ -0,0 +1,32 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const io = require("@actions/io");
+const TypeDoc = require("typedoc");
+
+async function run() {
+  try {
+    const tsconfigFilepath = core.getInput("tsconfig-filepath");
+    const entryPoints = core.getInput("entry-points");
+    const outputDir = "."
+    console.log(`entryPoints: ${entryPoints}`);
+
+    const app = new TypeDoc.Application();
+
+    app.bootstrap({
+      entryPoints: [entryPoints],
+      tsconfig: tsconfigFilepath,
+    });
+
+    const project = app.convert();
+
+    if (project) {
+      await app.generateDocs(project, outputDir);
+    }
+
+    core.setOutput("output", outputDir);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run()
