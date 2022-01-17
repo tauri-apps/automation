e0d1fe51a00ac5c5d4db19b986126f43c845f7f3
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-01-17 13:15:03 +0000

    back to script

--- update-typescript-docs/build.js
+++
@@ -1,3 +0,0 @@
-const { exec } = require('child_process');
-
-exec("yarn typedoc docs --out output --plugin typedoc-plugin-markdown --tsconfig 'docs/tsconfig.json' --hideInPageTOC true --hideBreadcrumbs true --githubPages false")
