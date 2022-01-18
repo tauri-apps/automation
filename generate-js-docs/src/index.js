aaaf314aebe7b31333d61334bb599e358dc340a1
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-01-18 19:55:49 +0000

    Remove async

--- generate-js-docs/src/index.js
+++ generate-js-docs/src/index.js
@@ -1,7 +1,7 @@
 const core = require("@actions/core");
 const TypeDoc = require("typedoc");
 
-async function run() {
+function run() {
   try {
     const tsconfigFilepath = core.getInput("tsconfig-filepath");
     const entryPoints = core.getInput("entry-points");
@@ -17,7 +17,7 @@ async function run() {
     const project = app.convert();
 
     if (project) {
-      await app.generateDocs(project, outputDir);
+      app.generateDocs(project, outputDir);
     }
 
     core.setOutput("output", outputDir);
