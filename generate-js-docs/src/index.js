68f642fc37e464862b2b4f8bf15ddf7e68d444f1
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-01-18 19:45:20 +0000

    Switch to generated output

---
+++ generate-js-docs/src/index.js
@@ -0,0 +1,29 @@
+const core = require("@actions/core");
+const TypeDoc = require("typedoc");
+
+async function run() {
+  try {
+    const tsconfigFilepath = core.getInput("tsconfig-filepath");
+    const entryPoints = core.getInput("entry-points");
+    const outputDir = "."
+
+    const app = new TypeDoc.Application();
+
+    app.bootstrap({
+      entryPoints: [entryPoints],
+      tsconfig: tsconfigFilepath,
+    });
+
+    const project = app.convert();
+
+    if (project) {
+      await app.generateDocs(project, outputDir);
+    }
+
+    core.setOutput("output", outputDir);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run()
