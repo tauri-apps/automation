594b5a3db839f15f8e87fbb87a8986446673cfa2
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-08-30 21:25:01 +0100

    dynamic message
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>

--- .github/actions/congrats/main.js
+++ .github/actions/congrats/main.js
@@ -1,12 +1,12 @@
 const github = require('@actions/github');
 
-fetch(process.env.DISCORD_WEBHOOK, {
+const payload = fetch(process.env.DISCORD_WEBHOOK, {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
   },
   body: JSON.stringify({
-    content: `\`${github.context.repo.repo}\` [\`test\`](https://tauri.app) merged! Thanks \`username\` 🥳`,
+    content: `[\`${github.context.payload.commits[0].message}\`](https://tauri.app) merged in \`${github.context.repo.repo}\`! Thanks \`${github.context.payload.commits[0].author.username}\` 🥳`,
     embeds: null,
     attachments: [],
     flags: 4,
