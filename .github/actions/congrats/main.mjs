de16a05664d8d9c3ebbcafdd4e28ccf7cf960498
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-08-30 20:37:15 +0100

    test webhook
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>

--- .github/actions/congrats/main.mjs
+++ .github/actions/congrats/main.mjs
@@ -1,1 +1,12 @@
-console.log('Hello, world');
+fetch(process.env.DISCORD_WEBHOOK, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    content: `\`repo name\` [\`test\`](https://tauri.app) merged! Thanks \`username\` 🥳`,
+    embeds: null,
+    attachments: [],
+    flags: 4,
+  }),
+});
