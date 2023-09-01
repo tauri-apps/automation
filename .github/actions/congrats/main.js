992d9cc15683c882c14b5687d203c269d193dae2
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-09-01 09:34:32 +0100

    Add documentation
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>

--- .github/actions/congrats/main.js
+++ .github/actions/congrats/main.js
@@ -1,6 +1,7 @@
 const github = require('@actions/github');
 
 const repo = github.context.payload.repository;
+// Only works for `push` type of actions for now as this code assumes data in `github.context.payload` is for a `push` type
 const commit = github.context.payload.commits[0];
 // Removes "Signed-off-by" text
 const commitMessage = commit.message.split('\n')[0];
@@ -10,17 +11,17 @@ const emoji = emojis[Math.floor(Math.random() * emojis.length)];
 
 const thanksMessages = [
   'Thanks',
-  'Gracias (thank you)',
-  'Merci (thank you)',
-  'Grazie (thank you)',
-  'Danke (thank you)',
-  'Bedankt (thank you)',
-  'Obrigado (thank you)',
-  '谢谢 (thank you)',
-  'شكرًا (thank you)',
-  'धन्यवाद (thank you)',
-  'ありがとう (thank you)',
-  'Mulţumesc (thank you)',
+  'Gracias, thank you,',
+  'Merci, thank you',
+  'Grazie, thank you',
+  'Danke, thank you',
+  'Bedankt, thank you',
+  'Obrigado, thank you',
+  '谢谢, thank you',
+  'شكرًا, thank you',
+  'धन्यवाद, thank you',
+  'ありがとう, thank you',
+  'Mulţumesc, thank you',
 ];
 const thanksMessage =
   thanksMessages[Math.floor(Math.random() * thanksMessages.length)];
--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # Tauri Automations
 
 Automations that are used across the `tauri-apps` GitHub organization.
+
+## Workflows
+
+### [`congrats`](.github/workflows/congrats.yml)
+
+Sends a message to a Discord channel with the last commit's message, author, and the repo it was posted in.
+
+Note: Currently only works for `push` triggers.
+
+**Inputs**
+
+- `DISCORD_WEBHOOK` (required): See workflow for documentation
+
+**Example**
+
+```yml
+on:
+  push:
+    branches: [dev]
+
+jobs:
+  congrats:
+    uses: tauri-apps/automation/.github/workflows/congrats.yml@main
+    secrets:
+      DISCORD_WEBHOOK: ${{ secrets.DISCORD_WEBHOOK }}
+```
+
+## Actions
+
+### [`congrats`](.github/actions/congrats)
+
+Used by the `congrats` **workflow**, prefer to use workflow directly.
