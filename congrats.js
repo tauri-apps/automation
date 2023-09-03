7ca0a69b9c7b1f3301b781e195b604eb24c0e531
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2023-09-03 12:38:19 +0100

    Ignore ci commits
    Signed-off-by: Lorenzo Lewis <lorenzo_lewis@icloud.com>

--- congrats.js
+++ congrats.js
@@ -1,27 +1,36 @@
 const github = require('@actions/github');
 
-const repo = github.context.payload.repository;
-// Only works for `push` type of actions for now as this code assumes data in `github.context.payload` is for a `push` type
-const commit = github.context.payload.commits[0];
-// Removes "Signed-off-by" text
-const commitMessage = commit.message.split('\n')[0];
+function congrats() {
+  const repo = github.context.payload.repository;
+  // Only works for `push` type of actions for now as this code assumes data in `github.context.payload` is for a `push` type
+  const commit = github.context.payload.commits[0];
 
-const emojis = ['🥳', '🎉', '💪', '🦾', '🧑‍💻'];
-const emoji = emojis[Math.floor(Math.random() * emojis.length)];
+  // Ignore CI messages
+  if (commit.message.startsWith('[ci]')) {
+    return;
+  }
+  // Removes "Signed-off-by" text
+  const commitMessage = commit.message.split('\n')[0];
 
-const thanksMessages = ['Thank you,'];
-const thanksMessage =
-  thanksMessages[Math.floor(Math.random() * thanksMessages.length)];
+  const emojis = ['🥳', '🎉', '💪', '🦾', '🧑‍💻'];
+  const emoji = emojis[Math.floor(Math.random() * emojis.length)];
 
-fetch(process.env.DISCORD_WEBHOOK, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    content: `[\`${commitMessage}\`](${commit.url}) merged in [\`${repo.name}\`](${repo.url})! ${thanksMessage} \`${commit.author.username}\` ${emoji}`,
-    embeds: null,
-    attachments: [],
-    flags: 4,
-  }),
-});
+  const thanksMessages = ['Thank you,'];
+  const thanksMessage =
+    thanksMessages[Math.floor(Math.random() * thanksMessages.length)];
+
+  fetch(process.env.DISCORD_WEBHOOK, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      content: `[\`${commitMessage}\`](${commit.url}) merged in [\`${repo.name}\`](${repo.url})! ${thanksMessage} \`${commit.author.username}\` ${emoji}`,
+      embeds: null,
+      attachments: [],
+      flags: 4,
+    }),
+  });
+}
+
+congrats();
